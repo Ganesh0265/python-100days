ae8200981f3ed04c64b573aa16439d8adf88175a
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-04-21 16:50:20 +0545

    Just added some comments

---
+++ 100_days_of_python_2025/0421_typeconversion.py
@@ -0,0 +1,11 @@
+a=123
+b="Ram"
+c="456"
+
+# here the variable a is storing numeric data while the data stored in c is string 
+#we can convert the data type in following way
+
+print(type(int(c)))
+
+print(str(a)+c+"and"+b)
+print(a+int(c))
