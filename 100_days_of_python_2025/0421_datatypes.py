b29243251dac077c722d7896cb68ee04570077e2
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-04-21 16:33:08 +0545

    Just added some comments

--- 100_days_of_python_2025/0421_datatypes.py
+++ 100_days_of_python_2025/0421_datatypes.py
@@ -1,3 +1,11 @@
-for i in range(1,10):
+for i in range(1,11):
     print (i)
+#the above example is of looping not data types but i just did it anyways for practice
 
+a="hello" #this is string data
+b=101 #this is numeric data
+c=True #this is boolean data
+
+print(a)
+print(b)
+print(c)
