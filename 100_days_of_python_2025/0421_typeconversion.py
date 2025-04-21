a2d6b7c6776dcc2912845b0157a90730f08a1a04
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-04-21 16:54:03 +0545

    Just added some comments

--- 100_days_of_python_2025/0421_typeconversion.py
+++ 100_days_of_python_2025/0421_typeconversion.py
@@ -5,7 +5,7 @@ c="456"
 # here the variable a is storing numeric data while the data stored in c is string 
 #we can convert the data type in following way
 
-print(type(int(c)))
+print(type(int(c)))#this is the type of data in variable c which is string
 
-print(str(a)+c+"and"+b)
-print(a+int(c))
+print(str(a)+c+"and"+b)#here variable a is converted into string from integer
+print(a+int(c)) #here variable c is converted into integer data type as int(c)
