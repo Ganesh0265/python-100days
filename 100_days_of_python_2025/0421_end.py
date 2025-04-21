19c57e7abef4f6e8d7dfa7ed0df441d619f42e21
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-04-21 17:01:00 +0545

    Just added some comments

---
+++ 100_days_of_python_2025/0421_end.py
@@ -0,0 +1,11 @@
+#WAP to add, sub, multiply and divide any two numbers.
+
+# these are practice of operators in python
+a=15
+b=3
+
+print("This is addition:", a+b)
+print("This is subtraction:", a-b)
+print("This is multiplication:", a*b)
+print("This is division:", a/b)
+print("This is division:", a//b) #this is integer division
