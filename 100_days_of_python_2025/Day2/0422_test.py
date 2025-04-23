0546f20793bbefa78dcc04202f6fc1dfee82a490
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-04-23 18:21:05 +0545

    Just added some comments

---
+++ 100_days_of_python_2025/Day2/0422_test.py
@@ -0,0 +1,11 @@
+#Arithmetic operators: +,-,*,/,%
+#Comparision or Relational operators: ==, >, <, >=, <=, !=
+#Logical operators: AND, OR, NOT
+#Bitwise operators: || 
+#Assignment operator: =
+
+first_name= "Ram"
+second_name= "Bam"
+
+print("Checking if the first name is same as second name: ",first_name==second_name)
+print("Checking if the first name is not same as second name: ",first_name!=second_name)
