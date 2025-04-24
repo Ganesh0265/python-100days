23cc2bcba0c5f4cdb993be5da25a1adadae25558
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-04-24 06:32:13 +0545

    Just added some comments

---
+++ 100_days_of_python_2025/Day1/0421_datatypes.py
@@ -0,0 +1,14 @@
+for i in range(1,11):
+    print(i)
+#the above example is of looping not data types but i just did it anyways for practice
+
+a="hello" #this is string data
+b=101 #this is numeric data
+c=True #this is boolean data
+
+print(a)
+print(b)
+print(c)
+
+# to check the data type through code 
+print("This is the type of data:",type(c))
