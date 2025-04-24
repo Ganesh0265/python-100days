19dba17f9681a261e839045b9df802fe2c44b5f0
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-04-24 09:59:37 +0545

    made the program to validate the input value error

--- 100_days_of_python_2025/Day4/0424_condition.py
+++ 100_days_of_python_2025/Day4/0424_condition.py
@@ -1,23 +1,28 @@
-#types of condition
+# Types of condition
 # if, if ...else, if ...elif ...else (nested condition)
 
-age=int(input("Enter Your Age:"))
-if age>=18:
-     print("You can vote!")
-else:
-     print("You cannot vote!")
+try:
+    age = int(input("Enter Your Age: "))
+    if age >= 18:
+        print("You can vote!")
+    else:
+        print("You cannot vote!")
+except ValueError:
+    print("Invalid Input!")
 
 
-#write a program to find if the user is child, teenager, adult or old
-age1=int(input("Enter Your Age:"))
-
-if age1>0 and age1<12:
-     print("You are a child.")
-elif age1>=12 and age1<20:
-     print("You are a teenager.")
-elif age1>=20 and age1<45:
-     print("You are an adult.")
-elif age1>=45 and age1<102:
-     print("You are Old.")
-else:
-     print("What a beautiful lie, now you must die...")
+# Program to find if the user is child, teenager, adult or old
+try:
+    age1 = int(input("Enter Your Age: "))
+    if age1 > 0 and age1 < 12:
+        print("You are a child.")
+    elif age1 >= 12 and age1 < 20:
+        print("You are a teenager.")
+    elif age1 >= 20 and age1 < 45:
+        print("You are an adult.")
+    elif age1 >= 45 and age1 < 102:
+        print("You are old.")
+    else:
+        print("What a beautiful lie, now you must die...")
+except ValueError:
+    print("Invalid Input!")
