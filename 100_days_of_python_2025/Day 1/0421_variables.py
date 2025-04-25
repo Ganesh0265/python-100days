b4ce9ff3d6004845d6cacf52dd317271d18dbee2
Ganesh Pariyar <105834278+Ganesh0265@users.noreply.github.com>
2025-04-25 12:09:19 +0545

    Delete 100_days_of_python_2025/Day 1/0421_variables.py

--- 100_days_of_python_2025/Day 1/0421_variables.py
+++
@@ -1,7 +0,0 @@
-a="this is string vlaue" # this is string variable
-b= 1234 # this is numeric variable
-c= True # this is boolean variable
-
-print("The number 1234 multiplied by 1234 is: ",b*b)
-print(a,"stored in string variable ""a"" ")
-print("All above information is ", c)
