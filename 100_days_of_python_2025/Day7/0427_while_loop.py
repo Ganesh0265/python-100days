c7b97d3b519187432fce5c8875a690a2349ceae8
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-04-27 14:47:35 +0545

    made some new comments and changes

--- 100_days_of_python_2025/Day7/0427_while_loop.py
+++ 100_days_of_python_2025/Day7/0427_while_loop.py
@@ -10,7 +10,11 @@ while condition:
     count=count+1
 print("Now the loop must stop as the only first fifth condition is True")
 
+#this code prints if it is odd and even beside the number upto 50
 i=1
-while i<=100:
-    print(i)
+while i<=50:
+    if i%2==0:
+        print(f"{i}(Even)")
+    else: 
+        print(f"{i}(Odd)")
     i=i+1
