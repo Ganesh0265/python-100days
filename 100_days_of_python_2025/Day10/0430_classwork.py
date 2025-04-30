fd128992bbec6c160e1a621d375cec740e45fdf0
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-04-30 23:47:53 +0545

    made some new comments and changes

---
+++ 100_days_of_python_2025/Day10/0430_classwork.py
@@ -0,0 +1,14 @@
+#This is a program to take input from use; name,mobile number and address then add it into a list.
+
+list1=[]
+
+name=input("Enter your name:")
+number=input("Enter your mobile number:")
+address=input("Enter your address:")
+list1.append(name)
+list1.append(number)
+list1.append(address)
+print(list1)
+
+
+
