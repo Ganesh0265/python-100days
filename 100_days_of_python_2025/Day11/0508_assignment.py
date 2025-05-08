85334cfafefeaa527c6038d2501bb269913b8f27
Ganesh Pariyar <ganeshpariyar265@gmail.com>
2025-05-08 08:21:34 +0545

    it's fun to write codes

---
+++ 100_days_of_python_2025/Day11/0508_assignment.py
@@ -0,0 +1,8 @@
+#Make a program to store students name and subject marks
+#store the dictionaries inside a list
+#delete student
+#search student
+#update student
+#enter student name, id, english subject marks, maths subject marks, computer subject marks, 
+#
+#
